e39e1d68188b8875d204623fa2247c70447a923f
Sam <sam@wanfamily.org>
2024-09-27 09:07:45 -0700

    9/27/24

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-from qubit_register import Qubit_Register
-from qubit_functions import Qubit_Functions
-from math import pi
+from quantum_adder import quantum_4bit_adder
 
 def main():
-    q1 = Qubit_Register(4)
-    q2 = Qubit_Register(4)
-    carry = Qubit_Register(4)
-    qf = Qubit_Functions()
-    
-    # Set q1 to represent 5 (0101 in binary)
-    q1[1].x_gate()
-    q1[3].x_gate()
-    
-    # Set q2 to represent 7 (0111 in binary)  
-    q2[1].x_gate()
-    q2[2].x_gate()
-    q2[3].x_gate()
-    print(q1.measure())
-    print(q2.measure())    
-    for i in range(3):
-        qf.cnot(q1[i], carry[i+1])
-        qf.cnot(q2[i], carry[i+1])
+    a, b = 7, 9
+    result = quantum_4bit_adder(a, b)
+    print(f"Quantum 4-Bit Addition: {a} + {b} = {result}")
 
-        qf.toffoli(carry[i], q1[i], q2[i])
+    test_cases = [(3, 5), (10, 6), (15, 15), (8, 7)]
+    for a, b in test_cases:
+        result = quantum_4bit_adder(a, b)
+        print(f"Quantum 4-Bit Addition: {a} + {b} = {result}")
     
-    qf.cnot(carry[3], q2[3])
-    result = int(''.join(str(x) for x in q2.measure()[::-1]), 2)
-    print("5 + 7 =", result)
-
 if __name__ == "__main__":
     main()
 
-    """
-    output: 
-    ['[1 0]', '[1 0]', '[1 0]']
-  ['[0.70710678 0.70710678]', '[0.70710678 0.70710678]', '[0.70710678 0.70710678]']
-  ['[1. 0.]', '[1. 0.]', '[1. 0.]']
-  [0, 0, 0]
-  5 + 7 = 15 ???
-  """
--- qubit.py
+++ qubit.py
@@ -1,95 +1,21 @@
 import numpy as np
-import scipy as sp
-import scipy.linalg
 
 class Qubit:
-    def __init__(self, state=np.array([1, 0])):
-        if type(state) == list:
-            if len(state) == 2:
-                state = np.array(state)
-            else:
-                raise ValueError("Invalid state")
-        if type(state) == np.ndarray:
-            self.state = state
-        self.Hadamard = 1./np.sqrt(2) * np.array([[1, 1],
-                                     [1, -1]])
-    def __str__(self):
-        return str(self.state)
-
-    def _normalize(self, state):
-        if isinstance(state, list):
-            state = np.array(state, dtype=complex)
-        if isinstance(state, np.ndarray):
-            if state.shape == (2,):
-                return state / np.linalg.norm(state)
-            else:
-                raise ValueError("Invalid state shape")
+    def __init__(self, state=None):
+        if state is None:
+            self.state = np.array([1, 0], dtype=complex)
         else:
-            raise ValueError("Invalid state type")
+            self.state = np.array(state, dtype=complex)
+        self.normalize()
 
-    def x_gate(self):
-        x_matrix = np.array([[0, 1], [1, 0]])
-        self.state = np.matmul(x_matrix, self.state)
-        return self
+    def normalize(self):
+        norm = np.linalg.norm(self.state)
+        if norm != 0:
+            self.state = self.state / norm
 
-    def y_gate(self):
-        y_matrix = np.array([[0, -1j], [1j, 0]])
-        self.state = np.matmul(y_matrix, self.state)
-        return self
-
-    def z_gate(self):
-        z_matrix = np.array([[1, 0], [0, -1]])
-        self.state = np.matmul(z_matrix, self.state)
-        return self
-
-    def h_gate(self):
-        self.state = np.matmul(self.Hadamard, self.state)
-        return self
-    
-    def s_gate(self):
-        s_matrix = np.array([[1, 0], [0, 1j]])
-        self.state = np.matmul(s_matrix, self.state)
-        return self
-    
-    def t_gate(self):
-        t_matrix = np.array([[1, 0], [0, np.exp(1j*np.pi/4)]])
-        self.state = np.matmul(t_matrix, self.state)
-        return self
-
-    def copy(self):
-        return Qubit(np.copy(self.state))
-
-    def __add__(self, other):
-        return Qubit(self.state + other.state)
-
-    def __mul__(self, other):
-        return Qubit(np.kron(self.state, other.state))
-    
-    def sqrt(self):
-        self.state = np.sqrt(self.state)
-        return self
-    
-    def __truediv__(self, other):
-        return Qubit(self.state / other.state)
-
-    def first(self):
-        return self.state[0]
-    
-    def second(self):
-        return self.state[1]
-    
-    def transpose(self):
-        self.state = np.transpose(self.state)
-        return self
-    
     def measure(self):
         prob_0 = np.abs(self.state[0])**2
-        if np.random.random() < prob_0:
-            self.state = np.array([1, 0], dtype=complex)
-            return 0
-        else:
-            self.state = np.array([0, 1], dtype=complex)
-            return 1
-    def h(self):
-        self.state = np.matmul(self.Hadamard, self.state)
-        return self
+        return 0 if np.random.random() < prob_0 else 1
+
+    def __str__(self):
+        return f"|ψ⟩ = {self.state[0]:.2f}|0⟩ + {self.state[1]:.2f}|1⟩"
